8ae1002c0edf06bb2b3397d3e86ef48808a62e66
Latn1ck's Archive | БЕЗ БАБ <95050681+Latn1ck@users.noreply.github.com>
2025-03-16 23:00:10 +0300

    Add files via upload

---
+++ 5.1.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset
+import os
+from torchvision import transforms
+import numpy as np
+import matplotlib.pyplot as plt
+class StanfordTreeBank:
+    '''
+    Wrapper for accessing Stanford Tree Bank Dataset
+    https://nlp.stanford.edu/sentiment/treebank.html
+    
+    Parses dataset, gives each token and index and provides lookups
+    from string token to index and back
+    
+    Allows to generate random context with sampling strategy described in
+    word2vec paper:
+    https://papers.nips.cc/paper/5021-distributed-representations-of-words-and-phrases-and-their-compositionality.pdf
+    '''
+    def __init__(self):
+        self.index_by_token = {} # map of string -> token index
+        self.token_by_index = []
+
+        self.sentences = []
+
+        self.token_freq = {}
+        
+        self.token_reject_by_index = None
+
+    def load_dataset(self, folder):
+        filename = os.path.join(folder, "datasetSentences.txt")
+
+        with open(filename, "r", encoding="latin1") as f:
+            l = f.readline() # skip the first line
+            
+            for l in f:
+                splitted_line = l.strip().split()
+                words = [w.lower() for w in splitted_line[1:]] # First one is a number
+                    
+                self.sentences.append(words)
+                for word in words:
+                    if word in self.token_freq:
+                        self.token_freq[word] +=1 
+                    else:
+                        index = len(self.token_by_index)
+                        self.token_freq[word] = 1
+                        self.index_by_token[word] = index
+                        self.token_by_index.append(word)
+        self.compute_token_prob()
+                        
+    def compute_token_prob(self):
+        words_count = np.array([self.token_freq[token] for token in self.token_by_index])
+        words_freq = words_count / np.sum(words_count)
+        
+        # Following sampling strategy from word2vec paper
+        self.token_reject_by_index = 1- np.sqrt(1e-5/words_freq)
+    
+    def check_reject(self, word):
+        return np.random.rand() > self.token_reject_by_index[self.index_by_token[word]]
+        
+    def get_random_context(self, context_length=5):
+        """
+        Returns tuple of center word and list of context words
+        """
+        sentence_sampled = []
+        while len(sentence_sampled) <= 2:
+            sentence_index = np.random.randint(len(self.sentences)) 
+            sentence = self.sentences[sentence_index]
+            sentence_sampled = [word for word in sentence if self.check_reject(word)]
+    
+        center_word_index = np.random.randint(len(sentence_sampled))
+        
+        words_before = sentence_sampled[max(center_word_index - context_length//2,0):center_word_index]
+        words_after = sentence_sampled[center_word_index+1: center_word_index+1+context_length//2]
+        
+        return sentence_sampled[center_word_index], words_before+words_after
+    
+    def num_tokens(self):
+        return len(self.token_by_index)
+        
+data = StanfordTreeBank()
+data.load_dataset("./stanfordSentimentTreebank/")
+
+print("Num tokens:", data.num_tokens())
+for i in range(5):
+    center_word, other_words = data.get_random_context(5)
+    print(center_word, other_words)
+num_negative_samples = 10
+
+class Word2VecPlain(Dataset):
+    '''
+    PyTorch Dataset for plain Word2Vec.
+    Accepts StanfordTreebank as data and is able to generate dataset based on
+    a number of random contexts
+    '''
+    def __init__(self, data, num_contexts=30000):
+        self.data=data
+        self.num_contexts=num_contexts
+    
+    def generate_dataset(self):
+        '''
+        Generates dataset samples from random contexts
+        Note: there will be more samples than contexts because every context
+        can generate more than one sample
+        '''
+        # TODO: Implement generating the dataset
+        # You should sample num_contexts contexts from the data and turn them into samples
+        # Note you will have several samples from one context
+        
+        
+    def __len__(self):
+        '''
+        Returns total number of samples
+        '''
+        # TODO: Return the number of samples
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        '''
+        Returns i-th sample
+        
+        Return values:
+        input_vector - torch.Tensor with one-hot representation of the input vector
+        output_index - index of the target word (not torch.Tensor!)
+        '''
+        # TODO: Generate tuple of 2 return arguments for i-th sample
+        resVector=torch.Tensor(1,self.__len__())
+        resOutput=0
+        return resVector, resOutput
+# dataset = Word2VecPlain(data, 10)
+# dataset.generate_dataset()
+# input_vector, target = dataset[3]
+# print("Sample - input: %s, target: %s" % (input_vector, int(target))) # target should be able to convert to int
