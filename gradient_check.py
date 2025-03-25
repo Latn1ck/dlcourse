b0d36c44992f3460c12bc7abaa653327daecc543
Latn1ck's Archive | БЕЗ БАБ <95050681+Latn1ck@users.noreply.github.com>
2025-03-26 00:41:49 +0300

    Add files via upload

---
+++ gradient_check.py
@@ -0,0 +1,78 @@
+import numpy as np
+import linear_classifer
+
+def check_gradient(f, x, delta=1e-5, tol = 1e-4):
+    '''
+    Checks the implementation of analytical gradient by comparing
+    it to numerical gradient using two-point formula
+
+    Arguments:
+      f: function that receives x and computes value and gradient
+      x: np array, initial point where gradient is checked
+      delta: step to compute numerical gradient
+      tol: tolerance for comparing numerical and analytical gradient
+
+    Return:
+      bool indicating whether gradients match or not
+    '''
+    
+    assert isinstance(x, np.ndarray)
+    assert x.dtype == float
+    
+    orig_x = x.copy()
+    fx, analytic_grad = f(x)
+    assert np.all(np.isclose(orig_x, x, tol)), "Functions shouldn't modify input variables"
+
+    assert analytic_grad.shape == x.shape
+    analytic_grad = analytic_grad.copy()
+
+    # We will go through every dimension of x and compute numeric
+    # derivative for it
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        ix = it.multi_index
+        analytic_grad_at_ix = analytic_grad[ix]
+        # TODO compute value of numeric gradient of f to idx
+        dx=np.zeros(x.shape)
+        dx[ix]=delta
+        fxPlus,gradPlus=f(x+dx)
+        fxMinus,gradMinus=f(x-dx)
+        numeric_grad_at_ix=(fxPlus-fxMinus)/(2*delta)
+        if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
+            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
+            return False
+
+        it.iternext()
+
+    print("Gradient check passed!")
+    return True
+
+        
+
+        
+def square(x):
+    return float(x*x), 2*x
+
+print(check_gradient(square, np.array([3.0])))
+
+def array_sum(x):
+    assert x.shape == (2,), x.shape
+    return np.sum(x), np.ones_like(x)
+
+print(check_gradient(array_sum, np.array([3.0, 2.0])))
+
+def array_2d_sum(x):
+    assert x.shape == (2,2)
+    return np.sum(x), np.ones_like(x)
+
+print(check_gradient(array_2d_sum, np.array([[3.0, 2.0], [1.0, 0.0]])))
+# TODO Implement softmax and cross-entropy for single sample
+probs = linear_classifer.softmax(np.array([-10, 0, 10]))
+
+# Make sure it works for big numbers too!
+probs = linear_classifer.softmax(np.array([1000, 0, 0]))
+assert np.isclose(probs[0], 1.0)
+
+probs = linear_classifer.softmax(np.array([-5, 0, 5]))
+print(probs)
+print(linear_classifer.cross_entropy_loss(probs, 1))
